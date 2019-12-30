8136d37691d40697aa5cdf1411d76c94133a3550
Anthony Holder <anthonyh202x@gmail.com>
2019-12-30 02:53:48 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -18,15 +18,16 @@ end
 
 def literal_aoh
   lit_aoh = [ 
-    {:name => "Don Gately" , :occupation => "Live-in Staff" }
-    {:name => , :occupation => }
-    {:name => , :occupation => }
-    {:name => , :occupation => }
-    {:name => , :occupation => }
+    {:name => "Don Gately" , :occupation => "Live-in Staff"},
+    {:name => "Joelle Van Dyne", :occupation => "Radio Personality" },
+    {:name => "Pat Monteseian", :occupation => "Staff"},
+    {:name => "Kate Gompert" , :occupation => "None" },
+    {:name => "Bruce Green", :occupation => "Fan of Mildred" }
     ]
 end
 
 def aoh_lookup(aoh, row, key)
+  
 end
 
 def aoh_update(aoh, row, key, new_value)
